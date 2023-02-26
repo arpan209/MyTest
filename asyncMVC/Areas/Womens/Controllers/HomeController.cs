ee4d06e87713f427434bfdabd773ec6f070e82b7
arpan209 <arpan209@gmail.com>
2023-02-26 18:18:29 -0500

    Women's area

---
+++ asyncMVC/Areas/Womens/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace asyncMVC.Areas.Womens.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Womens/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
