551c5f8d3805ceb29d99b73bb6bd9150c0cac216
arpan209 <arpan209@gmail.com>
2023-03-01 00:08:46 -0500

    Partial view

---
+++ asyncMVC/Controllers/MyController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace asyncMVC.Controllers
+{
+    public class MyController : Controller
+    {
+        // GET: My
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult student() {
+            string[] arr = { "CS Student", "ME Student", "PH Student", "EC Student", "EE Student" };
+            return PartialView("_student", arr);
+        }
+    }
+}
